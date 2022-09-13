1166e0910cc2288839e2e47ab4ea17b95812d32c
nathanlaguiar <nathanlaguiar02@gmail.com>
2022-09-12 20:35:08 -0400

    comment out line to demo GIT.

---
+++ main.py
@@ -0,0 +1,12 @@
+your_name = input("What's your name?")
+print("Wowie!!", your_name, "That's super duper, I hope we can get along! ^^")
+your_major = input("So, what is your major?",)
+print("Cool! Comp151 is really going to help you out with", your_major, "...Do your best! I believe in you! ^^")
+message1 = 'I said "I want single quotes here" '
+message2 = " You can't underestimate yourself. "
+message3 =  'Nothing can stop this sting!!''Not even a break!!''Are you not entertained!'
+print(message2)
+comment_to_user = f" {your_name} Comp151 will really help with {your_major}"
+print(comment_to_user)
+
+
